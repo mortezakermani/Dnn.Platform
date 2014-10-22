1158376f9440e716059526139846d0872aaf80df
Francesco Rivola <francesco.rivola@gmail.com>
2014-10-22 15:08:59 +0200

    DNN-5943 - Added IWorkflowActionManager

--- DNN Platform/Library/Entities/Content/Workflow/Actions/IWorkflowActionManager.cs
+++ DNN Platform/Library/Entities/Content/Workflow/Actions/IWorkflowActionManager.cs
@@ -19,18 +19,29 @@
 // DEALINGS IN THE SOFTWARE.
 #endregion
 
-using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
 
 namespace DotNetNuke.Entities.Content.Workflow.Actions
 {
-    // TODO: add documentation
-    [PrimaryKey("ActionId")]
-    [TableName("ContentWorkflowActions")]
-    public class WorkflowAction
+    /// <summary>
+    /// This class is responsible to manage Workflow Actions
+    /// </summary>
+    public interface IWorkflowActionManager
     {
-        public int ActionId { get; set; }
-        public int ContentTypeId { get; set; }
-        public string ActionType { get; set; }
-        public string ActionSource { get; set; }
+        /// <summary>
+        /// This method gets an instance of the IWorkflowAction associated to the content type and action type
+        /// </summary>
+        /// <param name="contentTypeId">Content Item Id</param>
+        /// <param name="actionType">Action type</param>
+        /// <returns>IWorkflowAction instance</returns>
+        IWorkflowAction GetWorkflowActionInstance(int contentTypeId, string actionType);
+
+        /// <summary>
+        /// This method registers a new workflow action
+        /// </summary>
+        /// <remarks>This method checks that the WorkflowAction Source implements the IWorkflowAction interface before register it</remarks>
+        /// <param name="workflowAction">Workflow action entity</param>
+        /// <exception cref="ArgumentException">Thrown if the ActionSource does not implement the IWorkflowAction interface</exception>
+        void RegisterWorkflowAction(WorkflowAction workflowAction);
     }
 }
